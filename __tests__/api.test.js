c34fc5fe073ac093517a4d72d78a4752cd594cd0
mehar-pa-45 <mehar.pavan77@gmail.com>
2026-04-28 22:32:18 +0530

    Update api.test.js

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -1,13 +1,25 @@
 const request = require("supertest");
 const app = require("../server");
 
-describe("Product API", () => {
+describe("Product API Tests", () => {
 
-  test("GET products", async () => {
+  test("GET / should return API Running", async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toBe("API Running");
+  });
+
+  test("GET products should return product list", async () => {
     const res = await request(app).get("/api/products");
 
     expect(res.statusCode).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
     expect(res.body.length).toBeGreaterThan(0);
+  });
+
+  test("Invalid route should return 404", async () => {
+    const res = await request(app).get("/wrongroute");
+    expect(res.statusCode).toBe(404);
   });
 
 });
