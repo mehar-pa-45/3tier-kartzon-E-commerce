76df486889398bbd4d3ffda318c1986a0838dcdc
mehar-pa-45 <mehar.pavan77@gmail.com>
2026-04-28 22:24:00 +0530

    Update server.test.js

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,11 +1,9 @@
 const request = require("supertest");
 const app = require("../server");
 
-describe("API Test", () => {
-
-  test("Server should respond 200", async () => {
+describe("Server Test", () => {
+  test("GET / should return 200", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
   });
-
 });
